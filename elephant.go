087f3f95200ee711b0bc994bca01ec5ef9f2945c
rheber <rheber@rediffmail.com>
2017-03-07 17:26:38 +1100

    Basic functionality complete

--- elephant.go
+++ elephant.go
@@ -1,18 +1,23 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"math/rand"
+	"os"
+	"os/exec"
 	"time"
 )
 
 func main() {
 	var (
-		seqLength uint
-		seed      int64
-		seq       []int
-		i         uint
+		seqLength  uint
+		seed       int64
+		seq        []byte
+		enteredSeq []byte
+		i          uint
+		err        error
 	)
 
 	flag.UintVar(&seqLength, "n", 10, "length of sequence to generate")
@@ -23,17 +28,36 @@ func main() {
 		panic("Sequence length must be positive")
 	}
 
-	seq = make([]int, seqLength)
+	seq = make([]byte, seqLength)
 	rand.Seed(seed)
 
 	// Initialise sequence.
 	for i = 0; i < seqLength; i++ {
-		seq[i] = rand.Intn(10)
+		seq[i] = '0' + byte(rand.Intn(10))
 	}
 
 	// Print sequence.
 	for i = 0; i < seqLength; i++ {
-		fmt.Print(seq[i])
+		fmt.Printf("%c", seq[i])
 	}
-	fmt.Printf("\nSeed: %v", seed) // Print seed for replication.
+	fmt.Print("\nHit enter to continue...")
+	fmt.Scanln()
+
+	// Clear screen.
+	var cmd = exec.Command("clear")
+	cmd.Stdout = os.Stdout
+	err = cmd.Run()
+	if err != nil {
+		panic(err) // Unable to clear screen.
+	}
+
+	fmt.Println("Enter the sequence:")
+	fmt.Scanln(&enteredSeq)
+
+	if bytes.Compare(seq, enteredSeq) != 0 {
+		fmt.Println("Incorrect!")
+	} else {
+		fmt.Println("Correct!")
+	}
+	fmt.Printf("Seed: %v", seed) // Print seed for replication.
 }
